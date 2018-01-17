bfb2cbfe692a29b12fd5706f51b1baa9487520ab
Demian <demian.mijucic@finconsgroup.com>
2018-01-17 11:35:40 +0100

    controllo lato BE univocità nome del cliente

--- services/cliente.service.js
+++ services/cliente.service.js
@@ -19,16 +19,36 @@ function addCliente(clienteParam) {
 		clienteParam.data_inizio_validita = Date.now();
     
     let newCliente = new Cliente(clienteParam);
-    console.log(newCliente);
     var query = {'_id':newCliente._id};
 
+    getClienteById(newCliente._id).then(cliente => {
 
-    Cliente.findOneAndUpdate(query, newCliente, {upsert:true, new: true}, function(err, doc){
+        countClientiByName(newCliente.nome_cliente).then(count => {
+
+            if ((count == 1 && cliente != null) || count == 0) {
+                Cliente.findOneAndUpdate(query, newCliente, { upsert: true, new: true }, function (err, doc) {
+                    if (err)
+                        deferred.reject(err.name + ': ' + err.message);
+                    else
+                        deferred.resolve(doc);
+                });
+            }
+            else
+                deferred.reject("Non è possibile inserire clienti con lo stesso nome")
+        });
+    });
+
+    return deferred.promise;
+}
+
+function countClientiByName(name){
+    var deferred = Q.defer();
+    
+    Cliente.find({"nome_cliente" : name}).count( function(err, doc){
         if (err)
             deferred.reject(err.name + ': ' + err.message);
         else 
             deferred.resolve(doc);
-        
     });
 
     return deferred.promise;
@@ -53,13 +73,11 @@ function getClienti() {
 function getClienteById(idParam) {
     var deferred = Q.defer();
 
-	Cliente.find({"_id" : {$eq : idParam}},function (err, cliente) {
-        if (err){
-          deferred.reject(err.name + ': ' + err.message);  
-      } else{
-        deferred.resolve(cliente);
-      }
-
+	Cliente.findById({"_id" : idParam },function (err, cliente) {
+        if (err)
+            deferred.reject(err.name + ': ' + err.message);  
+        else
+            deferred.resolve(cliente);
     });
 
     return deferred.promise;
